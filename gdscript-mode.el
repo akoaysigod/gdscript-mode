a57e2e05458a2d2c033c4fe0760d735a380e1ea1
Anthony Green <akoaysigod@gmail.com>
2015-02-14 17:00:14 -0500

    giving up for now

--- gdscript-mode.el
+++ gdscript-mode.el
@@ -4,7 +4,7 @@
 
 
 ;; (defvar gdscript-keywords
-;;   '("def" "var" "const" "return" "for" "if" "else" "elif"))
+;;   '("func" "var" "const" "return" "for" "if" "else" "elif"))
 
 ;; (defvar gdscript-keywords-regexp
 ;;   (mapconcat 'identity gdscript-keywords "\\|"))
@@ -22,42 +22,15 @@
 ;;                                     '(("\\|const\\|var" 1
 ;;                                        font-lock-keyword-face t)))))
 
-(eval-when-compile
-  (defconst gdscript-rx-constituents
-    (list
-     `(block-start          . ,(rx symbol-start
-                                   (or "func" "class" "if" "elif" "else" "try"
-                                       "except" "finally" "for" "while" "with")
-                                   symbol-end))
-     `(decorator            . ,(rx line-start (* space) ?@ (any letter ?_)
-                                   (* (any word ?_))))
-     `(defun                . ,(rx symbol-start (or "def" "class") symbol-end))
-     `(symbol-name          . ,(rx (any letter ?_) (* (any word ?_))))
-     `(open-paren           . ,(rx (or "{" "[" "(")))
-     `(close-paren          . ,(rx (or "}" "]" ")")))
-     `(simple-operator      . ,(rx (any ?+ ?- ?/ ?& ?^ ?~ ?| ?* ?< ?> ?= ?%)))
-     `(not-simple-operator  . ,(rx (not (any ?+ ?- ?/ ?& ?^ ?~ ?| ?* ?< ?> ?= ?%))))
-     `(operator             . ,(rx (or "+" "-" "/" "&" "^" "~" "|" "*" "<" ">"
-                                       "=" "%" "**" "//" "<<" ">>" "<=" "!="
-                                       "==" ">=" "is" "not")))
-     `(assignment-operator  . ,(rx (or "=" "+=" "-=" "*=" "/=" "//=" "%=" "**="
-                                       ">>=" "<<=" "&=" "^=" "|="))))
-    "Additional Python specific sexps for `python-rx'"))
+(defun gdscript-indent-line ()
+  (interactive)
+  
 
-(defmacro gdscript-rx (&rest regexps)
-  "Python mode specialized rx macro which supports common python named REGEXPS."
-  (let ((rx-constituents (append gdscript-rx-constituents rx-constituents)))
-    (cond ((null regexps)
-           (error "No regexp"))
-          ((cdr regexps)
-           (rx-to-string `(and ,@regexps) t))
-          (t
-           (rx-to-string (car regexps) t)))))
-
-
+  )
 
 
 (define-derived-mode gdscript-mode fundamental-mode "GDScript"
+  (setq-local indent-line-function 'gdscript-indent-line)
   (font-lock-add-keywords nil '("\\<\\(const\\|func\\|var\\)\\>")))
   ;; (setq-local comment-start "#")
   ;; (setq-local font-lock-defaults '(gdscript-font-lock)))
