1603eda00d7d78b72a7b286aa7045748ea95c65f
Tony <akoaysigod@gmail.com>
2015-02-21 14:20:20 -0500

    cleaning up

--- gdscript-mode.el
+++ gdscript-mode.el
@@ -18,28 +18,15 @@
     (,(regex-maker gdscript-builtin-words) 1 font-lock-type-face)
     ))
 
-(defun test-gdscript-newline-and-indent ()
-  (interactive)
-  (newline)
-  (insert-char ?  5))
-
 (defun gdscript-indent-line ()
   (interactive)
   )
-
-(defun wtf ()
-  (interactive)
-  (back-to-indentation)
-  (skip-chars-backward "\r\n\t ")
-  (print (looking-at ":")))
 
 (defun gdscript-should-indent ()
   (save-excursion
     (skip-chars-backward "\r\n\t ")
     (let ((char-eol (char-before (line-end-position))))
-      (progn
-        (char-equal ?\: char-eol)))
-      ))
+        (char-equal ?\: char-eol))))
 
 (defun gdscript-newline-and-indent ()
   (interactive)
@@ -47,18 +34,12 @@
     (newline)
     (if (gdscript-should-indent)
         (insert-char ?  (+ ci gdtab))
-      (insert-char ?  ci)))
+      (insert-char ?  ci))))
 
 (define-derived-mode gdscript-mode fundamental-mode "GDScript"
   (setq-local indent-line-function 'gdscript-indent-line)
   (setq-local font-lock-defaults '(gdscript-font-lock)))
 
- (provide 'gdscript-mode)
+(provide 'gdscript-mode)
 
 (add-to-list 'auto-mode-alist '("\\.gd\\'" . gdscript-mode))
-
- ;; (let ((char-of-eol (char-before (line-end-position))))
- ;;      (or (and char-of-eol (memq char-of-eol coffee-indenters-eol))
- ;;          (progn
- ;;            (back-to-indentation)
- ;;            (looking-at (coffee-indenters-bol-regexp)))))))
